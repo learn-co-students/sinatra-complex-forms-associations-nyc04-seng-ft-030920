739f72297a6086b775d1e9f73549341c48c099c0
replacem3nts <donny.landis@gmail.com>
2020-04-01 23:41:32 -0400

    completes the codealong portion

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,16 +5,22 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    else
+      @pet.owner = params["pet"]["owner"]
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
